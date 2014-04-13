802f278d31f165ddc9886f2a6e99a1e344d1e4ec
sleepwalking <2657202503@qq.com>
2014-04-13 17:11:57 +0800

    Spectrum class

---
+++ src/Container/_Spectrum.h
@@ -0,0 +1,27 @@
+RClass(_RTClassName)
+{
+    RInherit(RObject);
+    
+    int Size;
+    int NoPhase;
+    int HalfSymm;
+    int MagnType;
+    _T1* Magn;
+    _T1* Phse;
+};
+
+RTMethod(void, CDSP2_Spectrum, CtorSize, int Size);
+RTMethod(void, CDSP2_Spectrum, Resize, int Size);
+
+RTMethod(void, CDSP2_Spectrum, FromWave, void* Sorc, int Position);
+RTMethod(void, CDSP2_Spectrum, FromWaveW, void* Sorc, int Position);
+RTMethod(void, CDSP2_Spectrum, FromReal, _T1* Sorc);
+RTMethod(void, CDSP2_Spectrum, FromRealW, _T1* Sorc, _T1* Window);
+RTMethod(void, CDSP2_Spectrum, FromCplx, _T1* Real, _T1* Imag);
+
+RTMethod(void, CDSP2_Spectrum, ToWave, void* Dest, int Position);
+RTMethod(void, CDSP2_Spectrum, ToWaveA, void* Dest, int Position);
+RTMethod(void, CDSP2_Spectrum, ToReal, _T1* Sorc);
+RTMethod(void, CDSP2_Spectrum, ToRealA, _T1* Sorc);
+RTMethod(void, CDSP2_Spectrum, ToCplx, _T1* Real, _T1* Imag);
+
--- test/TestDSP/main.c
+++ test/TestDSP/main.c
@@ -1,5 +1,6 @@
 #include "Interface/Setting.h"
 #include "Container/InfWave.h"
+#include "Container/Spectrum.h"
 #include <RFNL.h>
 
 int main()
@@ -22,20 +23,21 @@ int main()
     RFree(temp);
     CDSP2_Wave_Double_Dtor(& twave);*/
     
+    
     int i, l;
     CDSP2_InfWave_Double twave;
     CDSP2_InfWave_Double_CtorSize(& twave, 10000);
     CDSP2_InfWave_Double_Relocate(& twave, 15000);
     
-    Double* temp = RAlloc_Double(6000);
+    Double* temp = RAlloc_Double(10000);
     Double* temp2 = RAlloc_Double(6000);
     Double* wind = RAlloc_Double(3000);
     CDSP2_VSet_Double(wind, 1, 3000);
     CDSP2_Hanning_Double(wind, wind, 3000);
-    CDSP2_IWave_Double_SetWindow(& twave, wind, 3000);
+    CDSP2_IWave_Double_SetWindow(& twave, wind, 2048);
     
     for(i = 0; i < 3000; i ++)
-        temp[i] = 0.2f * i;
+        temp[i] = sin(0.2f * i) + 1.0f;
 
     CDSP2_IWave_Double_Add(& twave, temp, 15500, 3000);
     CDSP2_InfWave_Double_Submit(& twave, 18500);
@@ -47,14 +49,35 @@ int main()
     
     CDSP2_IWave_Double_WAdd(& twave, temp, 19000);
     CDSP2_InfWave_Double_Submit(& twave, 20500);
-    l = CDSP2_InfWave_Double_Dump(& twave, temp2);
-    for(i = 0; i < 6000; i ++)
-        printf("%f\n", temp2[i]);
-    //printf("%d\n", l);
+    
+    
+    CDSP2_Spectrum_Double spec;
+    CDSP2_Spectrum_Double_CtorSize(& spec, 2048);
+    spec.MagnType = CDSP2_LinearMagn;
+    spec.HalfSymm = 0;
+    //float temp[2048];
+    //int i;
+    //for(i = 0; i < 2048; i ++)
+    //    temp[i] = sin(i * 0.1) + sin(i * 0.2);
+    CDSP2_Spectrum_Double_FromWave(& spec, & twave, 17000);
+    CDSP2_Spectrum_Double_ToWaveA(& spec, & twave, 22000);
+    CDSP2_InfWave_Double_Submit(& twave, 25000);
+    
+    l = CDSP2_InfWave_Double_Dump(& twave, temp);
+    for(i = 0; i < 10000; i ++)
+        printf("%f\n", temp[i]);
+/*
+    double Real[2048];
+    double Imag[2048];
+    CDSP2_InfWave_Double_Read(& twave, temp2, 17000, 2048);
+    CDSP2_RFFT_Double(Real, Imag, temp2, 11);
+    CDSP2_IRFFT_Double(temp, Real, Imag, 11);
+*/
     RFree(temp);
     RFree(temp2);
     RFree(wind);
     CDSP2_InfWave_Double_Dtor(& twave);
+    RDelete(& spec);
     return 0;
 }
 
