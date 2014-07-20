f6fe9cf62deff6a7ffddc568c5f7d48411db5d12
sleepwalking <2657202503@qq.com>
2014-07-20 21:38:10 +0800

    Energy estimation & Normalization

---
+++ src/Iterator/_NormIterfector.h
@@ -0,0 +1,35 @@
+#define _Wave _C(CDSP2_Wave, _, _T1)
+#define _IWave _C(CDSP2_IWave, _, _T1)
+#define _Gain _C(CDSP2_GainIterfector, _, _T1)
+
+RClass(_RTClassName)
+{
+    RInherit(RObject);
+    
+    //Public
+    _T1 Gain;
+    _T1 Intensity;
+    int HopSize;
+    
+    //Private
+    _Gain* Subsizer;
+    void* Wave;
+    int LastPosition;
+    int InitPosition;
+};
+
+RTMethod(void, CDSP2_NormIterfector, SetWave, void* Sorc);
+RTMethod(void, CDSP2_NormIterfector, SetOutWave, void* Sorc);
+RTMethod(void, CDSP2_NormIterfector, SetHopSize, int HopSize);
+RTMethod(void, CDSP2_NormIterfector, SetGain, int Gain);
+RTMethod(void, CDSP2_NormIterfector, SetIntensity, int Intensity);
+RTMethod(void, CDSP2_NormIterfector, SetPosition, int Position);
+
+RTMethod(void, CDSP2_NormIterfector, IterNextTo, int Position);
+RTMethod(void, CDSP2_NormIterfector, IterPrevTo, int Position);
+RTMethod(void, CDSP2_NormIterfector, PrevTo, int Position);
+
+#undef  _Wave
+#undef  _IWave
+#undef  _Gain
+
---
+++ src/_Energy.h
@@ -0,0 +1,4 @@
+RTFunc(_T1, CDSP2_EnergyFromWave, void* Sorc, int LBound, int Size);
+RTFunc(_T1, CDSP2_MeanEnergyFromWave, void* Sorc, int LBound, int Size);
+RTFunc(_T1, CDSP2_MeanEnergyFromWaveDB, void* Sorc, int LBound, int Size);
+
